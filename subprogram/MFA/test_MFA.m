12dd4852851659adb29cf6d3306947dd75f91927
ZheWANGGlobal <zhewangglobal@gmail.com>
2017-04-12 16:47:06 +0800

    initial commit

--- subprogram/MFA/test_MFA.m
+++ subprogram/MFA/test_MFA.m
@@ -23,32 +23,96 @@ p1=2^-8;
 % X = AR32_row;
 % X = CK64_row;
 
-indices=crossvalind('Kfold',Y,8);%进行随机分包
-correct1 = cell(1,8);
-correct2 = cell(1,8);
-for k=1:8                  %交叉验证k=10，10个包轮流作为测试集   
-   test = (indices == k);   %获得test集元素在数据集中对应的单元编号
-   train = ~test;           %train集元素的编号为非test元素的编号
-   x_trn=X(train,:);     %从数据集中划分出train样本的数据
-%    y_trn=Y(train);%获得样本集的测试目标，在本例中是实际分类情况
-   y_trn=Y(train);
-   x_tst=X(test,:);%test样本集
-   y_tst=Y(test);
-   
-   
- [eigenvectors_transMat,X_trn,X_tst]=sparse_MFA(x_trn,y_trn,x_tst); 
-   [out1]=cknear(knn,X_trn,y_trn,X_tst); 
-  Acc(k,1)=mean(out1==y_tst);
+% indices=crossvalind('Kfold',Y,8);%进行随机分包
+% correct1 = cell(1,8);
+% correct2 = cell(1,8);
+% for k=1:8                  %交叉验证k=10，10个包轮流作为测试集   
+%    test = (indices == k);   %获得test集元素在数据集中对应的单元编号
+%    train = ~test;           %train集元素的编号为非test元素的编号
+%    x_trn=X(train,:);     %从数据集中划分出train样本的数据
+% %    y_trn=Y(train);%获得样本集的测试目标，在本例中是实际分类情况
+%    y_trn=Y(train);
+%    x_tst=X(test,:);%test样本集
+%    y_tst=Y(test);
+%    
+%     [eigvectorPCA,eigvaluePCA] = PCA2(double(x_trn),1003);
+%  x_trn_PCA = double(x_trn)*eigvectorPCA;
+%   x_tst_PCA = double(x_tst)*eigvectorPCA; 
+%   
+%  [eigenvectors_transMat,X_trn,X_tst]=sparse_MFA(x_trn_PCA,y_trn,x_tst_PCA); 
+%    [out1]=cknear(knn,X_trn,y_trn,X_tst); 
+%   Acc(k,1)=mean(out1==y_tst);
+%     
+%  [eigenvectors_transMat,X_trn,X_tst]=sparse_MFA(x_trn,y_trn,x_tst); 
+%    [out1]=cknear(knn,X_trn,y_trn,X_tst); 
+%   Acc(k,1)=mean(out1==y_tst);
+%   
+%  [mapping,X_trn,X_tst] = MFA(x_trn_PCA,y_trn,x_tst_PCA);
+%  [out2]=cknear(knn,X_trn,y_trn,X_tst); 
+%  
+%   correct1{k} = (out1==y_tst);
+%  correct2{k} = (out2==y_tst);
+%  %correct(:,k)=(out2==y_tst);
+%  Acc(k,2)=mean(out2==y_tst);
+% 
+%  end
+
+for i=1:20
+    for j=1:7
+         index = find(Y==j);
+         index_trn = index(round(rand(1,4)*length(index)));
+         index_tst = index - index_trn;   
+         
+         
+    end
     
+     x_trn = X(index_trn,:);
+     y_trn=Y(index_trn);
+     x_tst=X(index_tst,:);
+     y_tst=Y(index_trn);
 
- [mapping,X_trn,X_tst] = MFA(x_trn_PCA,y_trn,x_tst_PCA);
- [out2]=cknear(knn,X_trn,y_trn,X_tst); 
- 
-  correct1{k} = (out1==y_tst);
- correct2{k} = (out2==y_tst);
- %correct(:,k)=(out2==y_tst);
- Acc(k,2)=mean(out2==y_tst);
+     [eigvectorPCA,eigvaluePCA] = PCA2(double(x_trn),1003);
+     x_trn_PCA = double(x_trn)*eigvectorPCA;
+     x_tst_PCA = double(x_tst)*eigvectorPCA; 
 
- end
+    [eigenvectors_transMat,X_trn,X_tst]=sparse_MFA(x_trn_PCA,y_trn,x_tst_PCA); 
+    [out1]=cknear(knn,X_trn,y_trn,X_tst); 
+    Acc(k,1)=mean(out1==y_tst);
+
+    [eigenvectors_transMat,X_trn,X_tst]=sparse_MFA(x_trn,y_trn,x_tst); 
+    [out1]=cknear(knn,X_trn,y_trn,X_tst); 
+    Acc(k,1)=mean(out1==y_tst);
+
+    [mapping,X_trn,X_tst] = MFA(x_trn_PCA,y_trn,x_tst_PCA);
+    [out2]=cknear(knn,X_trn,y_trn,X_tst); 
+
+    Acc(k,2)=mean(out2==y_tst);
+   
+    
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 mean(Acc)
